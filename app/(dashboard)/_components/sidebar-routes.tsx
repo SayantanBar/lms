1860d07519c8afcb684fbc196542db3d0c047db9
Sayantan Bar <sayantan22.main@gmail.com>
2024-01-20 23:37:37 +0530

    initial commit

---
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { usePathname } from "next/navigation";
+import SidebarItem from "./sidebar-item";
+import { Layout, Compass, List, BarChart } from "lucide-react";
+
+const guestRoutes = [
+  {
+    icon: Layout,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Compass,
+    label: "Browse",
+    href: "/search",
+  },
+];
+
+const teacherRoutes = [
+  {
+    icon: List,
+    label: "Courses",
+    href: "/teacher/courses",
+  },
+  {
+    icon: BarChart,
+    label: "Analytics",
+    href: "/teacher/analytics",
+  },
+];
+const SidebarRoutes = () => {
+  const pathname = usePathname();
+  const isTeacherPage = pathname?.includes("/teacher");
+  const routes = isTeacherPage ? teacherRoutes : guestRoutes;
+  return (
+    <div className="flex flex-col w-full">
+      {routes.map((route) => (
+        <SidebarItem
+          key={route.href}
+          label={route.label}
+          href={route.href}
+          icon={route.icon}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SidebarRoutes;
